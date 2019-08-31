07eb8c89c8c82e79e6ed6df934d1f23c0e57f6cb
Danny Seymour <danny.saseymour@gmail.com>
2019-08-30 20:55:36 -0400

    project files added

---
+++ granite_house/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace granite_house.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
